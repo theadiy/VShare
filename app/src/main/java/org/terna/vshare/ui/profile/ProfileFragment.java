501c7a4bfc60496635848b20459d7e4061a7e9c0
theadiy <sawantaditya44@gmail.com>
2020-01-24 11:25:11 -0500

    Added profile Video Upload button

--- app/src/main/java/org/terna/vshare/ui/profile/ProfileFragment.java
+++ app/src/main/java/org/terna/vshare/ui/profile/ProfileFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -16,17 +18,34 @@ import org.terna.vshare.R;
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
+    Button profileVideoUploadButton;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        final View root = inflater.inflate(R.layout.fragment_profile, container, false);
         //final TextView textView = root.findViewById(R.id.text_gallery);
+        profileVideoUploadButton = root.findViewById(R.id.profile_uploads_Button);
         profileViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 //textView.setText(s);
+
+                profileVideoUploadButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Toast.makeText(root.getContext(),"Uploading video",Toast.LENGTH_SHORT).show();
+
+
+
+                    }
+                });
+
+
+
             }
         });
         return root;
